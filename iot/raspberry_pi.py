4fe548ca0d0cd4639dbf12f4bc394fff8e1eeea5
Satya19112003 <ukalasapudi@gmail.com>
2026-01-29 10:21:15 +0530

    Update raspberry_pi.py

--- iot/raspberry_pi.py
+++ iot/raspberry_pi.py
@@ -1,1 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+LED = 18
+GPIO.setup(LED, GPIO.OUT)
+
+def lights_on():
+    GPIO.output(LED, GPIO.HIGH)
+
+def lights_off():
+    GPIO.output(LED, GPIO.LOW)
+
+if __name__ == "__main__":
+    lights_on()
+    time.sleep(5)
+    lights_off()
 
