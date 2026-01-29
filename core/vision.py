5ee8aa4182ef13e63e0daed21cd46cbc3ba6de28
Satya19112003 <ukalasapudi@gmail.com>
2026-01-29 10:26:31 +0530

    Add face recognition functionality using OpenCV

--- core/vision.py
+++ core/vision.py
@@ -1,1 +1,13 @@
+import cv2
+import face_recognition
+
+def recognize_face():
+    cam = cv2.VideoCapture(0)
+    ret, frame = cam.read()
+    cam.release()
+
+    faces = face_recognition.face_locations(frame)
+    if faces:
+        return True
+    return False
 
