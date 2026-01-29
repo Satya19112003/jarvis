0cf85b2213cdb3ef93245b57d422f0c3e34145b5
Satya19112003 <ukalasapudi@gmail.com>
2026-01-29 10:25:45 +0530

    Add basic UI setup using tkinter

--- ui/desktop_ui.py
+++ ui/desktop_ui.py
@@ -1,1 +1,19 @@
+import tkinter as tk
+
+def launch_ui():
+    root = tk.Tk()
+    root.title("JARVIS")
+    root.geometry("800x500")
+    root.configure(bg="black")
+
+    label = tk.Label(
+        root,
+        text="JARVIS ONLINE",
+        fg="cyan",
+        bg="black",
+        font=("Orbitron", 28)
+    )
+    label.pack(pady=40)
+
+    root.mainloop()
 
